a56a4cff582f3bd14646f75b7c78841b6290ccf7
Chirag <cjain67@gmail.com>
2020-06-07 17:24:52 -0700

    Added the navbar component

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Nav from './components/navbar.js';
+
+// css
+import 'bootstrap/dist/css/bootstrap.css';
+import '../assets/css/main.css'
+import '../assets/css/flickity.min.css'
+import '../assets/css/theme-1.css'
+import '../assets/css/index.css'
+
+// js
+import '../assets/js/analytics.js'
+import '../assets/js/all.js'
+// import '../assets/js/fbevents.js'
+import '../assets/js/jquery-3.4.1.min.js'
+import '../assets/js/popper.min.js'
+import '../assets/js/bootstrap.min.js'
+// import '../assets/js/flickity.pkgd.min.js'
+// import '../assets/js/main.js'
+// import '../assets/js/flickity-custom.js'
+
+
+class App extends Component {
+  
+  render() {
+    const slide_style = {
+      width: '61.8984px',
+       left: '1218.38px',
+    };
+    return (
+      <div>
+        <Nav/>
+      </div>          
+    );
+  }
+}
+
+export default App;
