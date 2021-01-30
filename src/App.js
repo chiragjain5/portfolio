c3f4a741f4e351e35907e02179e0f4951ca834d0
Chirag <cjain67@gmail.com>
2021-01-30 16:07:15 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ class App extends Component {
             <Nav/>
             <Switch>
               {/* <Redirect exact from = '/' to = '/about' /> */}
-              <Route exact path="/" exact component={About}/>
+              <Route exact path="" exact component={About}/>
               <Route exact path="/projects" component={Projects}/>
               <Route exact path="/resume" component={Resume}/>
              
